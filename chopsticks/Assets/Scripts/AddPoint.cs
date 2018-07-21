df66d63f633aed20bb3b5532cd710cb4bbc621ec
alpokorney <alpokorney@gmail.com>
2018-07-21 16:46:06 -0700

    mah stuff

---
+++ chopsticks/Assets/Scripts/AddPoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPoint : MonoBehaviour {
+
+    public int point = 0;
+
+    // Use this for initialization
+    void Start() { 
+       
+    }
+     
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.touchCount > 0)
+            print("Touched");
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint((Input.GetTouch(0).position)), Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                point++;
+                print("Clicked on " + gameObject);
+                //Destroy(hit.transform.gameObject);
+            }
+        }
+    }
+}
