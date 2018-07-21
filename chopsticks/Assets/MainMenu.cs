85f097eb9cb85f89e8f04676a4623092d4c51d94
Bryce Collins <brycecollins0@gmail.com>
2018-07-21 11:58:30 -0700

    More work on menu animations

--- chopsticks/Assets/MainMenu.cs
+++ chopsticks/Assets/MainMenu.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
 
+	public Animator animator;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,5 +14,29 @@ public class MainMenu : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void playLocalPressed(){
+		fadeOut();
+	}
+
+	public void playNetworkedPressed(){
+		fadeOut();
+	}
+
+	public void settingsPressed(){
+		fadeOut();
+	}
+
+	public void creditsPressed(){
+		fadeOut();
+	}
+
+	private void fadeOut(){
+		animator.SetTrigger("FadeOut");
+	}
+
+	private void fadeIn(){
+		animator.SetTrigger("FadeIn");
 	}
 }
