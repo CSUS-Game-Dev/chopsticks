0e606c2951147d8796b21dc4132df064c6214c5c
Bryce Collins <brycecollins0@gmail.com>
2018-07-21 14:43:32 -0700

    Main menu button update + credits page

---
+++ chopsticks/Assets/Menu/MainMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+	public Animator animator;
+	public MenuParticleScript particles;
+	public GameObject credits;
+	// Use this for initialization
+	public Button playButton, playNetworkedButton, settingsButton, creditsButton;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void playLocalPressed(){
+		fadeOut();
+	}
+
+	public void playNetworkedPressed(){
+		fadeOut();
+	}
+
+	public void settingsPressed(){
+		fadeOut();
+	}
+
+	public void creditsPressed(){
+		fadeOut();
+		credits.SetActive(true);
+	}
+
+	public void fadeOut(){
+		animator.SetTrigger("FadeOut");
+		buttonsEnabled(false);
+	}
+
+	public void fadeIn(){
+		animator.SetTrigger("FadeIn");
+		buttonsEnabled(true);
+	}
+
+	private void buttonsEnabled(bool status){
+		playButton.interactable = status;
+		playNetworkedButton.interactable = status;
+		settingsButton.interactable = status;
+		creditsButton.interactable = status;
+	}
+}
---
+++ chopsticks/Assets/Menu/MenuParticleScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MenuParticleScript : MonoBehaviour {
+
+	public RectTransform canvas;
+	public Camera mainCam;
+	public Vector2 screenSize;
+
+	private ParticleSystem particles;
+	public float distanceAboveScreen;
+
+	// Use this for initialization
+	void Start () {
+		Vector3 worldPos = mainCam.ScreenToWorldPoint(new Vector3(canvas.rect.width / 2, canvas.rect.height,0f));
+		transform.position = new Vector3(worldPos.x, worldPos.y + distanceAboveScreen, 0f);
+
+		particles = GetComponent<ParticleSystem>();
+
+		setupDefaultMaterial();
+		setParticleRunning(true); 
+	}
+	
+	public void setupDefaultMaterial(){
+		//particles.Stop();
+
+	}
+
+	public void setParticleRunning(bool running){
+		if(running){particles.Play();}
+		else{particles.Stop();}
+	}
+}
